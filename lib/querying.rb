452bf6a13b2e5101a56d3ad69226b49502a501e3
Edsel Caprice <caprice@mail.usf.edu>
2018-07-28 17:44:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "select author.name, subgenres.name
+  "select authors.name, subgenres.name
   from authors
   inner join series
   on authors.id = series.author_id
