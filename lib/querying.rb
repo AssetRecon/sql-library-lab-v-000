7646579ef9269b91b63e06642d8683d0277ea55a
Edsel Caprice <caprice@mail.usf.edu>
2018-07-28 17:30:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "select characters.species, count(characters.species)
   from characters
-  order by count(characters.species) desc
+  order by characters.species desc
   limit 1"
 end
 
