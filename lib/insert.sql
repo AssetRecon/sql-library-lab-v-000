d018bc322ce9227a300b999b7235c7d527799943
Edsel Caprice <caprice@mail.usf.edu>
2018-07-28 17:17:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,12 +24,20 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("nam
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("name7", "this is my motto", "elf", 1, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("name8", "this is my motto", "human", 1, 2);
 
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
+INSERT INTO character_books (book_id, character_id) VALUES (1,1);
+INSERT INTO character_books (book_id, character_id) VALUES (2,1);
+INSERT INTO character_books (book_id, character_id) VALUES (3,1);
+INSERT INTO character_books (book_id, character_id) VALUES (1,2);
+INSERT INTO character_books (book_id, character_id) VALUES (2,2);
+INSERT INTO character_books (book_id, character_id) VALUES (3,2);
+INSERT INTO character_books (book_id, character_id) VALUES (1,3);
+INSERT INTO character_books (book_id, character_id) VALUES (2,4);
 
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
-INSERT INTO character_books (book_id, character_id) VALUES ();
+INSERT INTO character_books (book_id, character_id) VALUES (4,1);
+INSERT INTO character_books (book_id, character_id) VALUES (5,1);
+INSERT INTO character_books (book_id, character_id) VALUES (6,1);
+INSERT INTO character_books (book_id, character_id) VALUES (4,2);
+INSERT INTO character_books (book_id, character_id) VALUES (5,2);
+INSERT INTO character_books (book_id, character_id) VALUES (6,2);
+INSERT INTO character_books (book_id, character_id) VALUES (4,3);
+INSERT INTO character_books (book_id, character_id) VALUES (5,4);
