45dd83a8ef797677e0a1f36aecfb95f66a32288c
Edsel Caprice <caprice@mail.usf.edu>
2018-07-28 17:22:04 +0000

    automatically backed up by learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,6 +1,3 @@
 update characters set species = "Martian" where id = 8
 
-select books.title, book.year
-from books
-where series_id = 1
-order by books.year
+select books.title, book.year from books where series_id = 1 order by books.year
